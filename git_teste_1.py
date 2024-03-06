09b01dfceb2b1e731c50f759f0f77e856a582336
guilherme <guilherme.2020102296@unicap.br>
2024-03-06 16:56:14 -0300

    Criando o arquivo que será deletado

--- git_teste_1.py
+++ git_teste_1.py
@@ -1,7 +1,4 @@
 print("Testando commits pelos comandos do curso de Git - Básico ao Avançado.")
-<<<<<<< HEAD
 
 print("Linha de código adicionada - testando git status")
-=======
 print("Testando o git pull ao editar arquivos - mudança feita diretamente pelo browser no github.")
->>>>>>> 58e1dea1a3a502714a695f7d78c13c0d4bfbf727
